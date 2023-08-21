b4ae1d576a11210a673a13186d5219bdcfde4b29
Divyansh garg <108273272+divg02@users.noreply.github.com>
2023-08-21 09:12:19 +0530

    Create PalindromeNumber1.cpp

---
+++ PalindromeNumber1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0))
+            return false;
+
+        int reversed = 0;
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+
+        // If the number of digits in x is odd, remove the middle digit in reversed.
+        return (x == reversed || x == reversed / 10);
+    }
+};
